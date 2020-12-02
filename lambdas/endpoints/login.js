b20dd85eb0ce9b070db66303517c35857e723afd
Cyber-Quest <wesleu9983@gmail.com>
2020-12-02 17:36:49 -0200

    if applied, add update JWT

--- lambdas/endpoints/login.js
+++ lambdas/endpoints/login.js
@@ -1,20 +1,16 @@
-const Responses = require('../common/API_Responses');
-const jwt = require('jsonwebtoken'); 
-
-const messageSecretJWT = process.env.messageSecretJWT;
+const Responses = require('../common/API_Responses'); 
+const { createJWT } = require("../common/JWT");
 
 const handler = async event => {  
     const control = JSON.parse(event.body); 
 
     if(control.user === 'wesley9983' && control.password === '123'){
       const id = 1; 
-      const token = jwt.sign({ id }, messageSecretJWT, {
-        expiresIn: 100  
-      });
+      const token = createJWT(id);
       return Responses._200({ auth: true, token: token});
     }
     
-    return Responses._400({user: control.password, auth: false}); 
+    return Responses._400({message:"usuário não existe!", auth: false}); 
 };
 
 exports.handler = handler;
